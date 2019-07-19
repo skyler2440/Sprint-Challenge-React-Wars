01b600de4b3881ba022f1e99c9eef9ff59c64fd3
skyler dowdy <skylerwebdev@gmail.com>
2019-07-19 13:04:16 -0400

    MVP
    Semantic styled table need to do QnA and work on stretch

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,29 +1,27 @@
-import React from 'react';
-import './App.css';
-import Grid from '../src/components/Grid'
+import React from "react";
+import "./App.css";
+import Grid from "../src/components/Grid";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
   return (
     <div className="App">
-      
-
-      
+      <h1>React Star Wars Project</h1>
       <section>
-        
         <Grid limit={10} />
       </section>
     </div>
   );
-}
+};
 const styleLink = document.createElement("link");
 styleLink.rel = "stylesheet";
-styleLink.href = "https://cdn.jsdelivr.net/npm/semantic-ui/dist/semantic.min.css";
+styleLink.href =
+  "https://cdn.jsdelivr.net/npm/semantic-ui/dist/semantic.min.css";
 document.head.appendChild(styleLink);
 export default App;
